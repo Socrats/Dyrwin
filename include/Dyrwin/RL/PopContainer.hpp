09c26d5f2e6415da7231c9f421e57c9b0aa49b9b
eliasfernandez <eliasfernandez.d@gmail.com>
2019-05-14 22:01:41 +0200

    moved declarations of PopContainer

--- include/Dyrwin/RL/PopContainer.hpp
+++ include/Dyrwin/RL/PopContainer.hpp
@@ -21,95 +21,36 @@ namespace EGTTools::RL {
         PopContainer() = default;
 
         PopContainer(const std::string &agent_type, size_t nb_agents, size_t nb_states, size_t nb_actions,
-                     size_t episode_length, double endowment, std::vector<double> args = {}) {
-            if (agent_type == "rothErev") {
-                for (unsigned i = 0; i < nb_agents; i++) {
-                    _agents.push_back(std::make_shared<Agent>(nb_states, nb_actions, episode_length, endowment));
-                }
-            } else if (agent_type == "rothErevLambda") {
-                if (args.size() < 2)
-                    throw std::invalid_argument("You must specify lambda and temperature as arguments");
-                for (unsigned i = 0; i < nb_agents; i++) {
-                    _agents.push_back(
-                            std::make_shared<RothErevAgent>(nb_states, nb_actions, episode_length, endowment, args[0],
-                                                            args[1]));
-                }
-            } else if (agent_type == "QLearning") {
-                if (args.size() < 3)
-                    throw std::invalid_argument("You must specify alpha, lambda and temperature as arguments");
-                for (unsigned i = 0; i < nb_agents; i++) {
-                    _agents.push_back(
-                            std::make_shared<QLearningAgent>(nb_states, nb_actions, episode_length, endowment, args[0],
-                                                             args[1],
-                                                             args[2]));
-                }
-            } else if (agent_type == "HistericQLearning") {
-                if (args.size() < 3)
-                    throw std::invalid_argument("You must specify alpha, beta and temperature as arguments");
-                for (unsigned i = 0; i < nb_agents; i++) {
-                    _agents.push_back(
-                            std::make_shared<HistericQLearningAgent>(nb_states, nb_actions, episode_length, endowment,
-                                                                     args[0], args[1],
-                                                                     args[2]));
-                }
-            } else if (agent_type == "BatchQLearning") {
-                if (args.size() < 2) throw std::invalid_argument("You must specify alpha and temperature as arguments");
-                for (unsigned i = 0; i < nb_agents; i++) {
-                    _agents.push_back(
-                            std::make_shared<BatchQLearningAgent>(nb_states, nb_actions, episode_length, endowment,
-                                                                  args[0], args[1]));
-                }
-            } else {
-                throw std::invalid_argument("Invalid agent type");
-            }
-        }
+                     size_t episode_length, double endowment, std::vector<double> args = {});
 
-        Agent &operator[](size_t index) {
-            return *_agents[index];
-        };
+        Agent &operator[](size_t index);
 
-        const Agent &operator[](size_t index) const {
-            return *_agents[index];
-        };
+        const Agent &operator[](size_t index) const;
 
-        Individual &operator()(size_t index) {
-            return _agents[index];
-        }
+        Individual &operator()(size_t index);
 
-        const Individual &operator()(size_t index) const {
-            return _agents[index];
-        }
+        const Individual &operator()(size_t index) const;
 
         typedef RL::Population::iterator iterator;
         typedef RL::Population::const_iterator const_iterator;
 
-        iterator begin() { return _agents.begin(); }
+        iterator begin();
 
-        iterator end() { return _agents.end(); }
+        iterator end();
 
-        const_iterator begin() const { return _agents.begin(); }
+        const_iterator begin() const;
 
-        const_iterator end() const { return _agents.end(); }
+        const_iterator end() const;
 
-        size_t size() const { return _agents.size(); }
+        size_t size() const;
 
-        void clear() { _agents.clear(); }
+        void clear();
 
-        void reset() {
-            for (auto& agent: _agents) agent->reset();
-        }
+        void reset();
 
-        std::string toString() const {
-            std::stringstream ss;
-            ss << "[";
-            for(size_t i = 0; i < size(); ++i) {
-                ss << _agents[i]->type() << ",";
-            }
-            ss << "]";
-            return ss.str();
-        }
+        std::string toString() const;
 
-        friend std::ostream &operator<<(std::ostream &o, PopContainer &r) { return o << r.toString(); }
+        friend std::ostream &operator<<(std::ostream &o, PopContainer &r);
     };
 }
 
