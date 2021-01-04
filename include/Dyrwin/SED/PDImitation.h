70ef83e6c65548208cb1a3a3ae7331fb2fea111f
eliasfernandez <eliasfernandez.d@gmail.com>
2021-01-04 19:34:01 +0100

    reformatted file

--- include/Dyrwin/SED/PDImitation.h
+++ include/Dyrwin/SED/PDImitation.h
@@ -20,74 +20,73 @@
  */
 
 namespace EGTTools {
-    class PDImitation {
-    public:
-        PDImitation(unsigned int generations, unsigned int pop_size, float beta, float mu,
-                    float coop_freq, const Eigen::Ref<const MatrixXd>& payoff_matrix);
+class PDImitation {
+ public:
+  PDImitation(unsigned int generations, unsigned int pop_size, float beta, float mu,
+              float coop_freq, const Eigen::Ref<const MatrixXd> &payoff_matrix);
 
-        ~PDImitation() = default;
+  ~PDImitation() = default;
 
-        float fermifunc(float beta, float a, float b);
+  float fermifunc(float beta, float a, float b);
 
-        float evolve(float beta);
+  float evolve(float beta);
 
-        float evolve(unsigned int runs, float beta);
+  float evolve(unsigned int runs, float beta);
 
-        std::vector<float> evolve(std::vector<float> betas);
+  std::vector<float> evolve(std::vector<float> betas);
 
-        std::vector<float> evolve(std::vector<float> betas, unsigned int runs);
+  std::vector<float> evolve(std::vector<float> betas, unsigned int runs);
 
-        inline void initialize_population(std::vector<unsigned int> &population);
+  inline void initialize_population(std::vector<unsigned int> &population);
 
+  // Getters
+  [[nodiscard]] unsigned int generations() const { return _generations; }
 
-        // Getters
-        unsigned int generations() { return _generations; }
+  [[nodiscard]] unsigned int pop_size() const { return _pop_size; }
 
-        unsigned int pop_size() { return _pop_size; }
+  [[nodiscard]] unsigned int nb_coop() const { return _nb_coop; }
 
-        unsigned int nb_coop() { return _nb_coop; }
+  [[nodiscard]] float mu() const { return _mu; }
 
-        float mu() { return _mu; }
+  [[nodiscard]] float beta() const { return _beta; }
 
-        float beta() { return _beta; }
+  [[nodiscard]] float coop_freq() const { return _coop_freq; }
 
-        float coop_freq() { return _coop_freq; }
+  [[nodiscard]] float result_coop_freq() const { return _final_coop_freq; }
 
-        float result_coop_freq() { return _final_coop_freq; }
+  [[nodiscard]] const MatrixXd &payoff_matrix() const { return _payoff_matrix; }
 
-        MatrixXd& payoff_matrix() {return _payoff_matrix;}
+  // Setters
+  void set_generations(unsigned int generations) { _generations = generations; }
 
-        // Setters
-        void set_generations(unsigned int generations) { _generations = generations; }
+  void set_pop_size(unsigned int pop_size) { _pop_size = pop_size; }
 
-        void set_pop_size(unsigned int pop_size) { _pop_size = pop_size; }
+  void set_beta(float beta) { _beta = beta; }
 
-        void set_beta(float beta) { _beta = beta; }
+  void set_coop_freq(float coop_freq) { _coop_freq = coop_freq; }
 
-        void set_coop_freq(float coop_freq) { _coop_freq = coop_freq; }
+  void set_mu(float mu) { _mu = mu; }
 
-        void set_mu(float mu) { _mu = mu; }
+  void set_payoff_matrix(const Eigen::Ref<const MatrixXd> &payoff_matrix) {
+    Eigen::Map<MatrixXd>(_payoff_matrix.data(), _payoff_matrix.rows(), _payoff_matrix.cols()) = payoff_matrix;
+  }
 
-        void set_payoff_matrix(const Eigen::Ref<const MatrixXd> &payoff_matrix) {
-            Eigen::Map<MatrixXd>(_payoff_matrix.data(), _payoff_matrix.rows(), _payoff_matrix.cols()) = payoff_matrix;
-        }
+ private:
+  unsigned int _generations, _pop_size, _nb_coop;
+  float _beta, _mu, _coop_freq, _final_coop_freq;
+  std::vector<unsigned int> _population;
+  MatrixXd _payoff_matrix;
 
-    private:
-        unsigned int _generations, _pop_size, _nb_coop;
-        float _beta, _mu, _coop_freq, _final_coop_freq;
-        std::vector<unsigned int> _population;
-        MatrixXd _payoff_matrix;
+  inline void _moran_step(unsigned int &p1, unsigned int &p2, int &gradient, float &ref,
+                          float &freq1, float &freq2, float &fitness1, float &fitness2,
+                          float &beta,
+                          std::vector<unsigned int> &population,
+                          std::uniform_int_distribution<unsigned int> dist,
+                          std::uniform_real_distribution<double> _uniform_real_dist);
 
-        inline void _moran_step(unsigned int &p1, unsigned int &p2, int &gradient, float &ref,
-                               float &freq1, float &freq2, float &fitness1, float &fitness2,
-                               float &beta,
-                               std::vector<unsigned int> &population,
-                               std::uniform_int_distribution<unsigned int> dist,
-                               std::uniform_real_distribution<double> _uniform_real_dist);
-
-        // Random generators
-        std::mt19937_64 _mt{EGTTools::Random::SeedGenerator::getInstance().getSeed()};
-    };
+  // Random generators
+  std::mt19937_64 _mt{EGTTools::Random::SeedGenerator::getInstance().getSeed()};
+};
 }
 
 #endif //DYRWIN_SED_PDIMITATION_H
